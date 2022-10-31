fcc377a11ec262dd4d91b77a31f996b6cfb9b54c
Alison Feldish <alison.feldish@gmail.com>
2022-10-31 18:59:00 -0400

    base files

---
+++ src/Pokecard.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Pokecard extends Component {
+    render() {
+        return (
+            <div className="Pokecard">
+                <h1>{this.props.name}</h1>
+                <img src="" />
+                <h2>{this.props.type}</h2>
+                <h2>{this.props.exp}</h2>
+            </div>
+        )
+    }
+}
+
+export default Pokecard;
