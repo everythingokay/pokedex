2975b582c3a5958c69c741a2ada81010b80d6a21
Alison Feldish <alison.feldish@gmail.com>
2022-11-01 18:21:11 -0400

    create card components

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import './Pokecard.css';
+
+const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
 class Pokecard extends Component {
     render() {
+        let imgSrc = `${POKE_API}${this.props.id}.png`;
         return (
             <div className="Pokecard">
                 <h1>{this.props.name}</h1>
-                <img src="" />
-                <h2>{this.props.type}</h2>
-                <h2>{this.props.exp}</h2>
+                <img src={imgSrc} />
+                <div>Type: {this.props.type}</div>
+                <div>Experience: {this.props.exp}</div>
             </div>
         )
     }
